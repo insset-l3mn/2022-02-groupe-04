daa68d3a1a27f7aa7dbab837b453218c34cb007e
Watisse Corentin <corentinwatisse@gmail.com>
2022-03-11 17:44:49 +0100

    REACT #6 : Commit de fin de cours
    A finir : les descriptions des différentes formation

--- front-end/src/Containers/Formation/Formation.js
+++ front-end/src/Containers/Formation/Formation.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import Background from '../../Components/Background/Background'
 import Navbar1 from '../../Components/Navbar1/Navbar1'
+import './Formation.css'
 
-
-const h1Style = {
-  textAlign: "center",
-  color: "white",
-  zIndex: "2",
-  paddingTop: "25px",
-  textDecoration: "underline 2px white"
-  
-  
-}
 
 export default function Formation() {
   return (
@@ -19,7 +10,97 @@ export default function Formation() {
         <Background />
         <Navbar1 />
 
-        <h1 style={h1Style}> Formation </h1>     
+        <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center", paddingTop:"20px"}}> Les métiers de l'audiovisuel  </h1>
+
+        <div className="container">
+          
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Animateur(trice) de 2D et 3D   </h1>
+            <p style={{ textAlign:"justify", paddingTop:'30px'}}> Maître du mouvement, l'animateur 2D et 3D est d'abord un artiste, spécialiste des images de synthèse. Cinéma, jeux vidéo, publicité ou site Internet, les projets d'animation ne manquent pas, et attirent de plus en plus de jeunes.</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Animateur(trice) de de radio et de télévision   </h1>
+            <p> TEST 2</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Assistant(te) de réalisateur(trice)  </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Cadreur(se)   </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Chargé(e) de production  </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Chef(fe)monteur(se) </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Directeur(trice) de la photographie   </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Ingénieur(e) du son  </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Journaliste-reporter d'images   </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Machiniste constructeur(trice) ou plateau   </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Mixeur(se) son   </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Photographe  </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Régisseur(se) générale   </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Scénariste   </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Scripte   </h1>
+            <p> TEST 1</p>
+        </section>
+
+        <section>
+          <h1 className="mb-1 h-1" style={{ color : "white", textAlign : "center"}}> Votre teste   </h1>
+            <p> Votre test vous oriente vers :</p>
+        </section>
+
+
+          </div>
+
+
+
+
       </>
   )
 }
